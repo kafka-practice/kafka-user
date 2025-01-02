b8620637bc960ebf1477f204c1400b1f8c6ae211
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-02 11:07:24 +0900

    ✨ feat : OpenTelemetry Collector 연결 설정

--- src/main/java/org/example/kafkauser/common/annotation/trace/config/TracingConfig.java
+++ src/main/java/org/example/kafkauser/common/annotation/trace/config/TracingConfig.java
@@ -20,8 +20,8 @@ import org.springframework.context.annotation.Configuration;
  * */
 @Configuration
 public class TracingConfig {
-//    @Value("${opentelemetry.tracer.exporter.otlp.endpoint}")
-//    private String url;
+    @Value("${opentelemetry.tracer.exporter.otlp.endpoint}")
+    private String url;
 
     @Value("${otel.resource.attributes.service.name}")
     private String serviceName;
@@ -33,9 +33,9 @@ public class TracingConfig {
     public OpenTelemetry openTelemetry() {
         // OtlpHttpSpanExporter 객체를 생성하여 OTLP HTTP 엔드포인트에 데이터 전송 설정.
         // `url`은 OTLP Collector 엔드포인트 주소.
-//        OtlpHttpSpanExporter spanExporter = OtlpHttpSpanExporter.builder()
-//                .setEndpoint(url) // OTLP HTTP 수집기가 위치한 URL을 설정합니다.
-//                .build();
+        OtlpHttpSpanExporter spanExporter = OtlpHttpSpanExporter.builder()
+                .setEndpoint(url) // OTLP HTTP 수집기가 위치한 URL을 설정합니다.
+                .build();
 
         // SdkTracerProvider 생성. 이는 OpenTelemetry SDK 의 트레이싱 구성 요소로, SpanProcessor와 리소스를 설정.
         SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
@@ -43,7 +43,7 @@ public class TracingConfig {
                         // 서비스의 이름을 설정하여 각 Span에 서비스 정보를 포함시킴
                         ResourceAttributes.SERVICE_NAME, serviceName)))
                 // BatchSpanProcessor를 추가하여 Span을 일괄 처리하고 `spanExporter`로 전송.
-//                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
                 .build();
 
         // OpenTelemetrySdk 객체 생성. 이를 통해 OpenTelemetry 추적 및 컨텍스트 전파 기능 구성.
